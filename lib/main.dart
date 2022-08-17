04eda646af80adf5c0324a4816831fcec85b7615
SyedAhmed334 <ali.sain334@gmail.com>
2022-08-18 00:46:49 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.only(left:10.0),
+            color:Colors.white,
+            child: Text('Hello World')
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
